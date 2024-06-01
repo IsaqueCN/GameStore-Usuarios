b86d6e530de7f3a57e71ebd2be5e6a0ccb8d4722
IsaqueCN <167625665+IsaqueCN@users.noreply.github.com>
2024-06-01 21:18:23 +0000

    consertnado coisas

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -2,32 +2,35 @@ let usernameInput = document.getElementById("usernameInput");
 let usernameErrorText = document.getElementById("errorUsername");
 let usernameDiv = document.getElementById("usernameDiv");
 let passwordInput = document.getElementById("passwordInput");
-let passwordErrorText = document.getElementById("errorPassword")
+let passwordErrorText = document.getElementById("errorPassword");
 let passwordDiv = document.getElementById("passwordDiv");
 let loginButton = document.getElementById("loginButton");
 
 let Cadastros; // Onde será guardado todas as contas
 try {
-    Cadastros = JSON.parse(localStorage.Cadastros)
+    Cadastros = JSON.parse(localStorage.Cadastros);
 } catch {
-    Cadastros = {}
-    localStorage.setItem("Cadastros", JSON.stringify(Cadastros))
+    Cadastros = {};
+    localStorage.setItem("Cadastros", JSON.stringify(Cadastros));
 }
 
 function checkUsername() {
     if (!Cadastros[usernameInput.value.toLowerCase()]) {
-        textError("Username", "Conta Não Encontrada!")
-        return false
+        textError("Username", "Conta Não Encontrada!");
+        return false;
     } else {
-        return true
+        return true;
     }
 }
 
 function checkPassword() {
-    console.log(Cadastros[usernameInput.value.toLowerCase()]["senha"])
-    if (Cadastros[usernameInput.value.toLowerCase()]["senha"] != passwordInput.value) {
+    console.log(Cadastros[usernameInput.value.toLowerCase()]["senha"]);
+    if (
+        Cadastros[usernameInput.value.toLowerCase()]["senha"] !=
+        passwordInput.value
+    ) {
         textError("Password", "Senha Incorreta.");
-        return false
+        return false;
     } else {
         return true;
     }
@@ -35,27 +38,39 @@ function checkPassword() {
 
 function resetErrors() {
     for (let i = 1; i <= 2; i++) {
-        let groupDiv
-        let errorElement
+        let groupDiv;
+        let errorElement;
         switch (i) {
-            case 1: groupDiv = usernameDiv; errorElement = usernameErrorText; break;
-            case 2: groupDiv = passwordDiv; errorElement = passwordErrorText; break;
+            case 1:
+                groupDiv = usernameDiv;
+                errorElement = usernameErrorText;
+                break;
+            case 2:
+                groupDiv = passwordDiv;
+                errorElement = passwordErrorText;
+                break;
         }
 
         errorElement.style.setProperty("display", "none", "important");
 
         [...groupDiv.children].forEach((element) => {
-            element.style.setProperty("border-color", "#e000e0", "important")
-        })
+            element.style.setProperty("border-color", "#e000e0", "important");
+        });
     }
 }
 
 function textError(errorElement, errorMessage) {
-    let groupDiv
+    let groupDiv;
 
     switch (errorElement) {
-        case 'Username': groupDiv = usernameDiv; errorElement = usernameErrorText; break;
-        case "Password": groupDiv = passwordDiv; errorElement = passwordErrorText; break;
+        case "Username":
+            groupDiv = usernameDiv;
+            errorElement = usernameErrorText;
+            break;
+        case "Password":
+            groupDiv = passwordDiv;
+            errorElement = passwordErrorText;
+            break;
     }
 
     try {
@@ -75,20 +90,23 @@ function textError(errorElement, errorMessage) {
 loginButton.addEventListener("click", () => {
     resetErrors();
     let validUser = checkUsername();
-    
+
     if (validUser) {
         if (checkPassword()) {
             // Se estiver tudo certo
-            loginButton.value = "Login Realizado!"
-            localStorage.setItem("Credentials", JSON.stringify({
-                "unchangedname": usernameInput.value,
-                "name": usernameInput.value.toLowerCase(),
-                "senha": passwordInput.value,
-            }))
+            loginButton.value = "Login Realizado!";
+            localStorage.setItem(
+                "Credentials",
+                JSON.stringify({
+                    unchangedname: usernameInput.value,
+                    name: usernameInput.value.toLowerCase(),
+                    senha: passwordInput.value,
+                }),
+            );
 
             //Aguardar 2 segundos e ir para a página de acesso
             setTimeout(() => {
-                window.location.href = "../AcessoPage/index.html"
+                window.location.href = "../DadosPage/index.html";
             }, 1000);
         }
     }
